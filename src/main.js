ce2eac2acf3ec46a2d7ee5f1727bdb2693899d10
ddvlhr <354874258@qq.com>
2020-04-03 15:01:21 +0800

    init login html

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 引入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
 
+axios.defaults.baseURL = ''
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
