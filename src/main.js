7ef85ce2cbbe359893b1d953b94263074445166a
TinkingIdea <354874258@qq.com>
2020-04-09 23:38:20 +0800

    goods_list add function complete.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
-
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -18,6 +23,23 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 时间格式化
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const year = dt.getFullYear()
+  const month = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const day = (dt.getDate() + '').padStart(2, '0')
+
+  const hour = (dt.getHours() + '').padStart(2, '0')
+  const minutes = (dt.getMinutes() + '').padStart(2, '0')
+  const seconds = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`
+})
 
 new Vue({
   router,
